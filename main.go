4be70431a61e6ad74c9023b1372790efcb11a395
Georg <georg.held@sauroter.de>
2019-08-30 09:47:07 +0200

    reading port from command line

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	if len(os.Args) < 2 {
+		log.Fatal("port required")
+		os.Exit(1)
+	}
+	arg := os.Args[1]
+	port := -1
+	fmt.Sscanf(arg, "%d", &port)
+	if port <= 0 {
+		log.Fatal("port number must be in range")
+		os.Exit(1)
+	}
+
+	log.Printf("started dump server on port %d\n", port)
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
 }
